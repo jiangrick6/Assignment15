b08e7bcd0d5f3bc0b2e5d0cd4696ff3b0634cab0
jiangrick6 <111881827+jiangrick6@users.noreply.github.com>
2024-04-22 02:45:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 const static_path = path.join(__dirname, "../public");
 const templates_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
-
+console.log("Templates Path:", templates_path);
 app.use(express.static(static_path));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,7 +23,7 @@ app.set("views", templates_path);
 hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index.hbs");
 });
 
 app.get("/attractions", (req, res) => {
