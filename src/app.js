079090045f3010cf61bb60bd0b351c1d40473ae5
Rick Jiang <Rick Jiang>
2024-04-21 15:30:47 -0400

    Final Project

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const http = require("http"); // Change to http module
 const hbs = require("hbs");
 const axios = require("axios"); // Import axios module
 
-const Contact = require("./models/contact");
+const Contact = require("./src/models/contact");
 
 const app = express();
-require("./db/conn");
+require("./src/db/conn");
 
 const port = process.env.PORT || 3000;
 
